aeca2da7c4d63dfb3c3455e51ce08ffc24d0ce9c
pavelbutkodev <pavelbutkodeveloper@gmail.com>
2021-05-30 01:26:38 +0300

    new update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import {
 	Route,
 	Switch,
-	Redirect
 } from "react-router-dom";
 import Main from "./layout/Main/Main";
 import Calc from "./layout/Calc/Calc";
@@ -10,13 +9,12 @@ function App() {
 	return (
 		<>
 			<Switch>
-				<Route path="/main">
+				<Route path="/">
 					<Main/>
 				</Route>
 				<Route exact path="/calc">
 					<Calc/>
 				</Route>
-				<Redirect from='/' to='main'/>
 			</Switch>
 		</>
 	);
