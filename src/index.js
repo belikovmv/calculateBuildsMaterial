06bbedcfa732768e172484a42077f8bdc5533627
pavelbutkodev <pavelbutkodeveloper@gmail.com>
2021-05-30 18:35:06 +0300

    new update

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {
-    BrowserRouter as Router,
+  HashRouter
 } from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router  basename={process.env.PUBLIC_URL}>
+        <HashRouter  basename={process.env.PUBLIC_URL}>
             <App/>
-        </Router>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
